651e5d877518acdf2bdc2e15dfcc618080f1b418
Leonardo Piekala <37380454+Piekala@users.noreply.github.com>
2022-09-29 11:45:11 -0300

    Update ListOfTeamMates.java

--- ListOfTeamMates.java
+++ ListOfTeamMates.java
@@ -1,19 +1,20 @@
 public class ListofTeamMates {
     public static void main(String[] args) {
-        ArrayList<TeamMate> list = new ArrayList<TeamMate>();
-        list.add(new TeamMate());
-        list.add(new TeamMate("1","Julio", true));
-        list.add(new TeamMate("2","Renan", true));
-        list.add(new TeamMate("3","Felipe"));
-        list.add(new TeamMate("4","Carlos"));
-        list.add(new TeamMate());
 
-        for(int i = 0; i < 6; i++){
-            list[i].printTeamMate();
+        ArrayList<TeamMate> user = new ArrayList<TeamMate>();
+        user.add(new TeamMate());
+        user.add(new TeamMate("1","Felipe"));
+        user.add(new TeamMate("2","Carlos"));
+        user.add(new TeamMate("3","Julio", true));
+        user.add(new TeamMate("4","Renan", true));
+        user.add(new TeamMate());
+
+        for(int i = 0; i < user.size(); i++){
+            user[i].printTeamMate();
         }
-        
-        for(int i = 0; i < 6; i++){
-            list[i].printTeamMate(list[i].getOnline);
+
+        for(int i = 0; i < user.size(); i++){
+            user[i].printTeamMate(user[i].getOnline);
         }
     }
 }
